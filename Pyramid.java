ab9212e7a2d8a752440d52201eb6087b08c1249d
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-01-12 06:50:40 +0530

    take limit from user

--- Pyramid.java
+++ Pyramid.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Pyramid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
 
     }
 }
