df9892398cd31cc7322ba06a316c1ace0f30f477
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-01-12 06:46:40 +0530

    add for loops

--- SimpleTraingle1.java
+++ SimpleTraingle1.java
@@ -4,8 +4,11 @@ public class SimpleTraingle1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-
-
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
