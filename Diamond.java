e20725f2056d63edbfb2a8a333ac741c34a68365
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-01-12 08:38:31 +0530

    add for loops to gain pattern

--- Diamond.java
+++ Diamond.java
@@ -4,7 +4,21 @@ public class Diamond {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < (n - i - 1); j++)
+                System.out.print(" ");
+            for (int k = 0; k < (2 * i + 1); k++)
+                System.out.print("*");
+            System.out.println();
+        }
+        int m = n - 2;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= i; j++)
+                System.out.print(" ");
+            for (int k = 1; k <= 2 * m + 1; k++)
+                System.out.print("*");
+            m = m - 1;
+            System.out.println();
+        }
     }
 }
