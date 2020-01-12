8ed5bf8652697b6b246b12b91a5c727ef3fce062
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-01-12 06:36:36 +0530

    change System.out.println() to System.out.print() to make pattern

--- SimpleTraingle.java
+++ SimpleTraingle.java
@@ -6,10 +6,9 @@ public class SimpleTraingle {
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                System.out.println("*");
+                System.out.print("*");
             }
+            System.out.println();
         }
-
     }
-
 }
